c769c1d1ca2f1d2ac1cce310142858b406433284
Vladislav Kitov <vlkitov@gmail.com>
2021-04-18 16:39:39 +0300

    Added Swift lessons files

---
+++ Swift_Lessons/firstLesson.playground/Pages/firstLesson.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+var welcomeMessage: String
+welcomeMessage = "Привет!"
+
+welcomeMessage = "123"
+
+print(welcomeMessage)
+
+let three: Int = 3
+let point = 0.14159
+let pi: Double = Double(three) + point // Преобразование типа переменной 
+
+print(pi)
+print("Pi = \(pi)")
+
+var g: Int = 129
+var s: String? = "World"
+
+var futureTeacher: String?
+futureTeacher = "Mr Adams"
+
+print("Future teacher \(futureTeacher!)")
+
+
+var students: [String] = ["Vlad", "Max", "Anna"]
